4b34a8a18788826eca7cf5dcdf3464df3b4556b6
lukasz.przytocki <lukasz.przytocki@gmail.com>
2019-05-23 22:29:11 +0200

    Added logic to finish task.

--- src/GuessingNumbers.java
+++ src/GuessingNumbers.java
@@ -3,13 +3,26 @@ import java.util.Scanner;
 public class GuessingNumbers {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int i=0;
+        int i;
+        int counter=0;
 
-        while (i<100 || i >200){
+        do{
             System.out.println("Zgadnij liczbę: ");
             i = scan.nextInt();
+            counter++;
+
+            if(i<100){
+                System.out.println("Liczba jest za mała ");
+            }else if(i>200){
+                System.out.println("Liczba jest za duża");
+            }else if(i%3 != 0){
+                System.out.println("Liczba nie jest podzielna przez 3");
+
+            }
         }
-        System.out.println("Zgadłeś, graulacje! " + i);
+        while ((i<100 || i >200) || i%3!=0);
+
+        System.out.printf("Zgadłeś za %d razem. Gratulacje!. Twoja liczba to %d.", counter,i);
 
     }
 }
